0719a7fd829623423e40dd2d1cb40a68c20f05bb
herotl2005 <nguyendanghung2008@gmail.com>
2016-09-09 11:33:32 +0700

    update parser
    update parser

--- rabbit.js
+++ rabbit.js
@@ -4,13 +4,10 @@
 var req = require('request');
 var Promise = require('bluebird');
 var request = Promise.promisify(req);
-
 var pageSize = 1000;
-
 
 var ServiceHelper = function (queue) {
     var self = this;
-
     var getChannelSummary = function () {
         var requestOption = {
             url: 'http://0.0.0.0:5000/api/v1/channel/' + pageSize
@@ -28,14 +25,11 @@ var ServiceHelper = function (queue) {
     };
 
     var pushItems = function (items) {
-
         for (var i = 0; i < items.length; i++) {
             var item = items[i];
             var ex = item.source;
-
             var msg = JSON.stringify(item);
             console.log('queue:' + queue.name);
-
             queue.publish(ex, queue.name, new Buffer(msg));
             console.log(" [x] Sent %s", msg);
         }
@@ -49,23 +43,19 @@ var ServiceHelper = function (queue) {
             var body = JSON.parse(data.body);
             return body.channels;
         };
-
         return request(requestOption).then(parserChannels).then(pushItems);
-
     };
     var getItems = function (pages) {
-
         return Promise.map(pages, function (page) {
             console.log('page' + page);
             return getItemsOfPageAndPush(page);
         });
     };
-
+    
     self.pushToQueue = function () {
         getChannelSummary()
             .then(getItems);
     };
-
 };
 
 module.exports = ServiceHelper;
