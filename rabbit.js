68439a95479b1a60b4804cf2e9654cb61fe447bc
herotl2005 <nguyendanghung2008@gmail.com>
2016-09-09 11:44:33 +0700

    update
    update

--- rabbit.js
+++ rabbit.js
@@ -5,55 +5,55 @@ var request = Promise.promisify(req);
 var pageSize = 1000;
 
 var ServiceHelper = function (queue) {
-  var self = this;
-  var parserChannelData = function (data) {
-    if (data == null || data == undefined) return;
-    var dataObject = JSON.parse(data.body);
-    var pages = [];
-    for (var i = 0; i < dataObject.totalPage; i++) {
-        pages.push(i);
-    }
-    return pages;
-  };
-  var getChannelSummary = function () {
-    var requestOption = {
-        url: 'http://0.0.0.0:5000/api/v1/channel/' + pageSize
+    var self = this;
+    var parserPages = function (data) {
+        if (data == null || data == undefined) return;
+        var dataObject = JSON.parse(data.body);
+        var pages = [];
+        for (var i = 0; i < dataObject.totalPage; i++) {
+            pages.push(i);
+        }
+        return pages;
     };
-    return request(requestOption).then(parserChannelData)
-  };
+    var getChannelSummary = function () {
+        var requestOption = {
+            url: 'http://0.0.0.0:5000/api/v1/channel/' + pageSize
+        };
+        return request(requestOption).then(parserPages)
+    };
 
-  var pushItems = function (items) {
-    for (var i = 0; i < items.length; i++) {
-      var item = items[i];
-      var ex = item.source;
-      var msg = JSON.stringify(item);
-      console.log('queue:' + queue.name);
-      queue.publish(ex, queue.name, new Buffer(msg));
-      console.log(" [x] Sent %s", msg);
-    }
- };
-  var parserChannels = function (data) {
-     var body = JSON.parse(data.body);
-     return body.channels;
- };
- var getItemsOfPageAndPush = function (page) {
-   var requestOption = {
-      url: 'http://0.0.0.0:5000/api/v1/channels/' + page
+    var pushChannelsToQueue = function (channels) {
+        for (var i = 0; i < channels.length; i++) {
+            var item = channels[i];
+            var ex = item.source;
+            var msg = JSON.stringify(item);
+            console.log('queue:' + queue.name);
+            queue.publish(ex, queue.name, new Buffer(msg));
+            console.log(" [x] Sent %s", msg);
+        }
     };
-   return request(requestOption).then(parserChannels).then(pushItems);
-  };
-  
-  var getItems = function (pages) {
-    return Promise.map(pages, function (page) {
-      console.log('page' + page);
-      return getItemsOfPageAndPush(page);
-    });
-  };
-  
-  self.pushToQueue = function () {
-    getChannelSummary()
-    .then(getItems);
-  };
+    var parserChannels = function (data) {
+        var body = JSON.parse(data.body);
+        return body.channels;
+    };
+    var getItemsOfPageAndPush = function (page) {
+        var requestOption = {
+            url: 'http://0.0.0.0:5000/api/v1/channels/' + page
+        };
+        return request(requestOption).then(parserChannels).then(pushChannelsToQueue);
+    };
+
+    var getItems = function (pages) {
+        return Promise.map(pages, function (page) {
+            console.log('page' + page);
+            return getItemsOfPageAndPush(page);
+        });
+    };
+
+    self.pushToQueue = function () {
+        getChannelSummary()
+            .then(getItems);
+    };
 };
 
 module.exports = ServiceHelper;
